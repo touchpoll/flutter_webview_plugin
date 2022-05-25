13839fd305249f22e6dfc9de504327373ca80ec3
Arik Achmad <arikarik.ae@gmail.com>
2022-05-25 10:19:14 +0700

    remove unused import and deprecated method

--- lib/src/webview_scaffold.dart
+++ lib/src/webview_scaffold.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_webview_plugin/src/javascript_channel.dart';
